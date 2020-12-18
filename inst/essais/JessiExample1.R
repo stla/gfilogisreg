138c9ecab4a64b81d46b8633ccabd211fc39dc8f
stla <laurent_step@yahoo.fr>
2020-12-18 18:01:11 +0100

    Jessi example 1

---
+++ inst/essais/JessiExample1.R
@@ -0,0 +1,17 @@
+library(gfilogisreg)
+
+x <- rep(c(-2, -1, 0, 1, 2), times = 5L)
+X <- model.matrix(~ x)
+n <- length(x)
+y <- c(matrix(c(
+  1, 0, 0, 0, 0,
+  1, 1, 1, 0, 0,
+  1, 1, 0, 0, 0,
+  1, 1, 1, 1, 0,
+  1, 1, 1, 1, 1
+), nrow = 5L, ncol = 5L, byrow = TRUE))
+
+glm(y ~ x, family = binomial())
+
+gf <- gfilogisreg(y ~ x, N = 2000)
+gfiConfInt(~ -`(Intercept)`/x, gf)
