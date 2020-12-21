ab68d0d46aee0416ff7c88a8558897d9ae6c05a4
stla <laurent_step@yahoo.fr>
2020-12-21 10:29:21 +0100

    BBoptim

--- inst/essais/BBoptim01.R
+++ inst/essais/BBoptim01.R
@@ -1,4 +1,4 @@
-library(numDeriv)
+library(BB)
 
 expit <- function(x) 1 / (1+exp(-x))
 logit <- function(u) log(u/(1-u))
@@ -12,6 +12,7 @@ P <- structure(c(-0.816496580927726, -0.408248290463863, 0, 0.408248290463863,
 ), .Dim = c(4L, 2L))
 b <- c(-0.497055737731043, 1.5763637399835, -1.66156026677388,
        0.582252264521417)
+
 f <- function(uv){
   vecx <- P %*% logit(uv) + b
   prod(dlogis(vecx)) * prod(dlogit(uv))
@@ -27,24 +28,22 @@ grl_i <- function(uv, i){
 grl <- function(uv){
   c(grl_i(uv, 1L), grl_i(uv, 2L))
 }
-
-grad(minus_logf, c(0.5,0.5))
--grl(c(0.5, 0.5))
-
 gr <- function(uv){
   f(uv) * grl(uv)
 }
-grad(f, c(0.5,0.5))
-gr(c(0.5, 0.5))
+
+# for the initial values
+B <- c(0.456112464221194, 0.206538736468621)
+
 
 # umax ####
-B <- c(0.456112464221194, 0.206538736468621)
-opt <- optim(
+opt <- BBoptim(
   par = expit(B),
-  fn = function(uv) -f(uv),
-  gr = function(uv) -gr(uv),
-  method = "L-BFGS-B",
-  lower = c(1e-16, 1e-16), upper = c(1-1e-16, 1-1e-16)
+  fn = f,
+  gr = gr,
+  lower = c(1e-16, 1e-16),
+  upper = c(1-1e-16, 1-1e-16),
+  control = list(maximize = TRUE, trace = FALSE)
 )
 opt$convergence
 mu <- opt[["par"]]
@@ -55,59 +54,57 @@ vmin <- c(NA_real_, NA_real_)
 #
 init <- expit(B)
 init[1] <- mu[1]/2
-opt <- optim(
+opt <- BBoptim(
   par = init,
-  #fn = function(uv) f(uv)^0.25 * (uv[1] - mu[1]),
   fn = function(uv) -(-minus_logf(uv) + 4*log(mu[1] - uv[1])),
   gr = function(uv) -grl(uv) + 4 * c(1/(mu[1] - uv[1]), 0),
-  method = "L-BFGS-B",
   lower = c(1e-16, 1e-16),
-  upper = c(mu[1]-1e-16, 1-1e-16)
+  upper = c(mu[1]-1e-16, 1-1e-16),
+  control = list(maximize = FALSE, trace = FALSE)
 )
 opt$convergence
-vmin[1] <- f(opt[["par"]])^0.25 * (opt[["par"]][1] - mu[1])
+vmin[1] <- -exp(-opt[["value"]]/4) # f(opt[["par"]])^0.25 * (opt[["par"]][1] - mu[1])
 #
 init <- expit(B)
 init[2] <- mu[2]/2
-opt <- optim(
+opt <- BBoptim(
   par = init,
   fn = function(uv) -(-minus_logf(uv) + 4*log(mu[2] - uv[2])),
   gr = function(uv) -grl(uv) + 4 * c(0, 1/(mu[2] - uv[2])),
-  method = "L-BFGS-B",
   lower = c(1e-16, 1e-16),
-  upper = c(1-1e-16, mu[2]-1e-16)
+  upper = c(1-1e-16, mu[2]-1e-16),
+  control = list(maximize = FALSE, trace = FALSE)
 )
 opt$convergence
-vmin[2] <- f(opt[["par"]])^0.25 * (opt[["par"]][2] - mu[2])
+vmin[2] <- -exp(-opt[["value"]]/4) # f(opt[["par"]])^0.25 * (opt[["par"]][2] - mu[2])
 
 # vmax ####
 vmax <- c(NA_real_, NA_real_)
 #
 init <- expit(B)
 init[1] <- (mu[1]+1)/2
-opt <- optim(
+opt <- BBoptim(
   par = init,
-#  fn = function(uv) -f(uv) * (uv[1] - mu[1])^4,
-  fn = function(uv) -(-minus_logf(uv) + 4*log(uv[1] - mu[1])),
-  gr = function(uv) -grl(uv) + 4 * c(1/(mu[1] - uv[1]), 0),
-  method = "L-BFGS-B",
+  fn = function(uv) -minus_logf(uv) + 4*log(uv[1] - mu[1]),
+  gr = function(uv) grl(uv) - 4 * c(1/(mu[1] - uv[1]), 0),
   lower = c(mu[1]+1e-16, 1e-16),
-  upper = c(1-1e-16, 1-1e-16)
+  upper = c(1-1e-16, 1-1e-16),
+  control = list(maximize = TRUE, trace = FALSE)
 )
 opt$par
-vmax[1] <- exp(-opt[["value"]]/4) # f(opt[["par"]])^0.25 * (opt[["par"]][1] - mu[1])
+vmax[1] <- exp(opt[["value"]]/4) # f(opt[["par"]])^0.25 * (opt[["par"]][1] - mu[1])
 #
 init <- expit(B)
 init[2] <- (mu[2]+1)/2
-opt <- optim(
+opt <- BBoptim(
   par = init,
-  fn = function(uv) -(-minus_logf(uv) + 4*log(uv[2] - mu[2])),
-  gr = function(uv) -grl(uv) + 4 * c(0, 1/(mu[2] - uv[2])),
-  method = "L-BFGS-B",
+  fn = function(uv) -minus_logf(uv) + 4*log(uv[2] - mu[2]),
+  gr = function(uv) grl(uv) - 4 * c(0, 1/(mu[2] - uv[2])),
   lower = c(1e-16, mu[2]+1e-16),
-  upper = c(1-1e-16, 1-1e-16)
+  upper = c(1-1e-16, 1-1e-16),
+  control = list(maximize = TRUE, trace = FALSE)
 )
-vmax[2] <- exp(-opt[["value"]]/4)
+vmax[2] <- exp(opt[["value"]]/4)
 
 
 # simulations ####
